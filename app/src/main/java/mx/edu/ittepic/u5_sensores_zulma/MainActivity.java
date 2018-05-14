00622614d076421f3f55de4903abd182a4d38f0f
Zulma <zuiscoronello@ittepic.edu.mx>
2018-05-14 03:53:42 -0600

    actualizado con recycler

--- app/src/main/java/mx/edu/ittepic/u5_sensores_zulma/MainActivity.java
+++ app/src/main/java/mx/edu/ittepic/u5_sensores_zulma/MainActivity.java
@@ -1,34 +1,57 @@
 package mx.edu.ittepic.u5_sensores_zulma;
 
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ContextMenu;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
-import android.widget.AdapterView;
-import android.hardware.SensorManager;
-import android.hardware.Sensor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SensorManager sensorManager;
-    TextView textView = null;
-    @Override protected void onCreate(Bundle savedInstanceState)
-    {
+    RecyclerView recyclerView;
+    ProductAdapter adapter;
+
+    List<DataProvider> productList;
+
+    SensorManager sensorManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView1);
-        sensorManager = (SensorManager)this.getSystemService(SENSOR_SERVICE);
+        productList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recycler_view);
+
+        sensorManager= (SensorManager)this.getSystemService(SENSOR_SERVICE);
         List<Sensor> lista = sensorManager.getSensorList(Sensor.TYPE_ALL);
         StringBuilder data = new StringBuilder();
-        for(int i=1; i<lista.size(); i++){
-            data.append("Nombre :"+lista.get(i).getName() + "\n");
-            data.append("Fabricante:"+lista.get(i).getVendor() + "\n");
-            data.append("Version :"+lista.get(i).getVersion() + "\n\n");
+
+
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Aquí va la adición de elementos a la lista
+
+        for(int i=1; i< lista.size(); i++){
+            productList.add(new DataProvider(
+                    "Nombre    :"+ lista.get(i).getName(),
+                    "Fabricante:"+lista.get(i).getVendor(),
+                    "Version:"+lista.get(i).getVersion(),
+                    "Rango maximo:"+lista.get(i).getMaximumRange(),
+                    "Retraso minimo:"+lista.get(i).getMinDelay(),
+                    "Potencia:"+lista.get(i).getPower()));
         }
-        textView.setText(data);
+        adapter = new ProductAdapter(this,productList);
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
